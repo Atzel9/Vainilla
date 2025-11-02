982b819b3400cd29783ca75e2a3adfa5c20e74e7
Atzel9 <axelperez23@cbtis52.edu.mx>
2025-11-01 19:44:50 -0600

    (WIP)Función de agregar ingredientes hecho

--- js/receta.js
+++ js/receta.js
@@ -72,6 +72,100 @@ bscIng.addEventListener('input', () => {
     }
 });
 
+//a. -> Agregar ingredientes
+
+//función para agregar el contenedor
+function agregarIngrediente(id, ingrediente){
+    const contIngrediente = document.getElementById("ingredientes-seleccionados");
+    const nuevoContIng = document.createElement("div");
+    nuevoContIng.classList.add("div-ing-seleccionado");
+    nuevoContIng.setAttribute("data-id", id);
+    nuevoContIng.setAttribute("data-ingrediente", ingrediente);
+
+    //Crear los elementos que estarán dentro del div
+    //Span
+    const spanNombre = document.createElement("span");
+    spanNombre.textContent = ingrediente;
+    spanNombre.classList.add("span-ing");
+    //Input number
+    const inputNum = document.createElement("input");
+    inputNum.type = "number";
+    inputNum.min = "1";
+    inputNum.value = "1";
+    inputNum.classList.add("ipt-cantidad-ing");
+    //Input tipo de unidad
+    const tipUnidad = document.createElement("select");
+    tipUnidad.classList.add("tipo-unidad");
+
+    const valores = [
+        { valor: 'g', texto: 'g' },
+        { valor: 'mg', texto: 'mg' },
+        { valor: 'kg', texto: 'kg' },
+        { valor: 'oz', texto: 'oz' },
+        { valor: 'lb', texto: 'lb' },
+        { valor: 'l', texto: 'l' },
+        { valor: 'ml', texto: 'ml' },
+        { valor: 'fl oz', texto: 'fl oz' },
+        { valor: 'tsp', texto: 'cucharadita (tsp)' },
+        { valor: 'tbsp', texto: 'cucharada (tbsp)' },
+        { valor: 'cup', texto: 'taza (cup)' },
+        { valor: 'pinch', texto: 'pizca' },
+        { valor: 'dash', texto: 'dash' },
+        { valor: 'unidad', texto: 'unidad(es)' } // para huevos, vainas, hojas, etc.
+    ];
+
+    valores.forEach(unidad => {
+        const opcion = document.createElement("option");
+        opcion.value = unidad.valor;
+        opcion.textContent = unidad.texto;
+        tipUnidad.appendChild(opcion);
+    });
+    //Input cerrar
+    const eliminarIng = document.createElement("button");
+    eliminarIng.classList.add("btn-eliminar-ing");
+    eliminarIng.type = "button";
+    eliminarIng.name = "btnIngrediente";
+    eliminarIng.innerHTML = "<i class=\"ph ph-x\"></i>";
+
+    //Eliminar contenedor
+    eliminarIng.addEventListener('click', () => {
+        const idOg = nuevoContIng.getAttribute("data-id");
+        const listaIngId = document.querySelector(`.lista-ing[data-id="${idOg}"]`);
+        if(listaIngId) {
+            listaIngId.style.display = "";
+        }
+
+        nuevoContIng.remove();
+    });
+
+    //Anidar los elementos
+    nuevoContIng.appendChild(spanNombre);
+    nuevoContIng.appendChild(inputNum);
+    nuevoContIng.appendChild(tipUnidad);
+    nuevoContIng.appendChild(eliminarIng);
+
+    contIngrediente.appendChild(nuevoContIng);
+}
+
+//Evento de cuando el usuario le da click al ingrediente
+const btnAgregar = document.querySelectorAll(".agregar-ingrediente");
+
+btnAgregar.forEach(boton => {
+    boton.addEventListener('click', function() {
+        console.log("Click");
+        //Buscar el ancestro que tenga la clase en el parametro
+        const OpcionIng = this.closest(".lista-ing");
+
+        //Obtener los atributos
+        const id = OpcionIng.getAttribute("data-id");
+        const nombre = OpcionIng.getAttribute("data-nombre");
+
+        //Agregar esos atributos a los parametros de la función y se cree el objeto
+        agregarIngrediente(id, nombre);
+
+        OpcionIng.style.display = "none";
+    });
+});
 
 function anadirPaso() {
     let nuevoDiv = document.createElement("div"); //crear div
